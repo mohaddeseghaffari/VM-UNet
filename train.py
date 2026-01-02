e38c2c77bbf907be9bdd8cfe439ae5d85c4332fa
mohaddeseghaffari <142239820+mohaddeseghaffari@users.noreply.github.com>
2026-01-02 20:37:23 +0330

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,5 @@
 import torch
 from torch.utils.data import DataLoader
-import timm
-from datasets.dataset import NPY_datasets
 from tensorboardX import SummaryWriter
 from models.vmunet.vmunet import VMUNet
 
@@ -11,10 +9,10 @@ import sys
 
 from utils import *
 from configs.config_setting import setting_config
+from datasets.dataset import ISIC17Dataset
 
 import warnings
 warnings.filterwarnings("ignore")
-
 
 
 def main(config):
@@ -25,10 +23,8 @@ def main(config):
     checkpoint_dir = os.path.join(config.work_dir, 'checkpoints')
     resume_model = os.path.join(checkpoint_dir, 'latest.pth')
     outputs = os.path.join(config.work_dir, 'outputs')
-    if not os.path.exists(checkpoint_dir):
-        os.makedirs(checkpoint_dir)
-    if not os.path.exists(outputs):
-        os.makedirs(outputs)
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    os.makedirs(outputs, exist_ok=True)
 
     global logger
     logger = get_logger('train', log_dir)
@@ -37,27 +33,20 @@ def main(config):
 
     log_config_info(config, logger)
 
-
-
-
-
     print('#----------GPU init----------#')
     os.environ["CUDA_VISIBLE_DEVICES"] = config.gpu_id
     set_seed(config.seed)
     torch.cuda.empty_cache()
 
-
-
-
-
     print('#----------Preparing dataset----------#')
-    train_dataset = NPY_datasets(config.data_path, config, train=True)
+    # چون دیتاست شما کوچک است، همان validation را برای train استفاده می‌کنیم
+    train_dataset = ISIC17Dataset(config.data_path, config, train=True)
     train_loader = DataLoader(train_dataset,
                                 batch_size=config.batch_size, 
                                 shuffle=True,
                                 pin_memory=True,
                                 num_workers=config.num_workers)
-    val_dataset = NPY_datasets(config.data_path, config, train=False)
+    val_dataset = ISIC17Dataset(config.data_path, config, train=False)
     val_loader = DataLoader(val_dataset,
                                 batch_size=1,
                                 shuffle=False,
@@ -65,11 +54,7 @@ def main(config):
                                 num_workers=config.num_workers,
                                 drop_last=True)
 
-
-
-
-
-    print('#----------Prepareing Model----------#')
+    print('#----------Preparing Model----------#')
     model_cfg = config.model_config
     if config.network == 'vmunet':
         model = VMUNet(
@@ -81,50 +66,39 @@ def main(config):
             load_ckpt_path=model_cfg['load_ckpt_path'],
         )
         model.load_from()
-        
-    else: raise Exception('network in not right!')
+    else:
+        raise Exception('network in not right!')
     model = model.cuda()
 
     cal_params_flops(model, 256, logger)
 
-
-
-
-
-    print('#----------Prepareing loss, opt, sch and amp----------#')
+    print('#----------Preparing loss, optimizer, scheduler----------#')
     criterion = config.criterion
     optimizer = get_optimizer(config, model)
     scheduler = get_scheduler(config, optimizer)
-
-
-
-
 
     print('#----------Set other params----------#')
     min_loss = 999
     start_epoch = 1
     min_epoch = 1
 
+    # فقط تست و ذخیره تصاویر
     if config.only_test_and_save_figs:
         checkpoint = torch.load(config.best_ckpt_path, map_location=torch.device('cpu'))
         model.load_state_dict(checkpoint)
         config.work_dir = config.img_save_path
-        if not os.path.exists(config.work_dir + 'outputs/'):
-            os.makedirs(config.work_dir + 'outputs/')
+        os.makedirs(config.work_dir + 'outputs/', exist_ok=True)
         loss = test_one_epoch(
-                val_loader,
-                model,
-                criterion,
-                logger,
-                config,
-            )
+            val_loader,
+            model,
+            criterion,
+            logger,
+            config,
+        )
         return
 
-
-
-
+    # resume
     if os.path.exists(resume_model):
-        print('#----------Resume Model and Other params----------#')
         checkpoint = torch.load(resume_model, map_location=torch.device('cpu'))
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
@@ -132,17 +106,12 @@ def main(config):
         saved_epoch = checkpoint['epoch']
         start_epoch += saved_epoch
         min_loss, min_epoch, loss = checkpoint['min_loss'], checkpoint['min_epoch'], checkpoint['loss']
-
-        log_info = f'resuming model from {resume_model}. resume_epoch: {saved_epoch}, min_loss: {min_loss:.4f}, min_epoch: {min_epoch}, loss: {loss:.4f}'
+        log_info = f'Resuming model from {resume_model}. resume_epoch: {saved_epoch}, min_loss: {min_loss:.4f}, min_epoch: {min_epoch}, loss: {loss:.4f}'
         logger.info(log_info)
-
-
-
 
     step = 0
     print('#----------Training----------#')
     for epoch in range(start_epoch, config.epochs + 1):
-
         torch.cuda.empty_cache()
 
         step = train_one_epoch(
@@ -159,13 +128,13 @@ def main(config):
         )
 
         loss = val_one_epoch(
-                val_loader,
-                model,
-                criterion,
-                epoch,
-                logger,
-                config
-            )
+            val_loader,
+            model,
+            criterion,
+            epoch,
+            logger,
+            config
+        )
 
         if loss < min_loss:
             torch.save(model.state_dict(), os.path.join(checkpoint_dir, 'best.pth'))
@@ -181,23 +150,25 @@ def main(config):
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
                 'scheduler_state_dict': scheduler.state_dict(),
-            }, os.path.join(checkpoint_dir, 'latest.pth')) 
+            }, os.path.join(checkpoint_dir, 'latest.pth')
+        )
 
+    # تست نهایی
     if os.path.exists(os.path.join(checkpoint_dir, 'best.pth')):
         print('#----------Testing----------#')
-        best_weight = torch.load(config.work_dir + 'checkpoints/best.pth', map_location=torch.device('cpu'))
+        best_weight = torch.load(os.path.join(checkpoint_dir, 'best.pth'), map_location=torch.device('cpu'))
         model.load_state_dict(best_weight)
         loss = test_one_epoch(
-                val_loader,
-                model,
-                criterion,
-                logger,
-                config,
-            )
+            val_loader,
+            model,
+            criterion,
+            logger,
+            config,
+        )
         os.rename(
             os.path.join(checkpoint_dir, 'best.pth'),
             os.path.join(checkpoint_dir, f'best-epoch{min_epoch}-loss{min_loss:.4f}.pth')
-        )      
+        )
 
 
 if __name__ == '__main__':
