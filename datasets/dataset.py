b581ae368874adab36bdd4407193f9d37a5dff3a
mohaddeseghaffari <142239820+mohaddeseghaffari@users.noreply.github.com>
2026-01-02 20:43:31 +0330

    Update dataset.py

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -11,29 +11,19 @@ def is_image_file(filename):
     IMG_EXT = [".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff"]
     return any(filename.lower().endswith(ext) for ext in IMG_EXT)
 
-# ------------------ Augmentation functions ------------------
-def random_rot_flip(image, label):
-    k = np.random.randint(0, 4)
-    image = np.rot90(image, k)
-    label = np.rot90(label, k)
-    axis = np.random.randint(0, 2)
-    image = np.flip(image, axis=axis).copy()
-    label = np.flip(label, axis=axis).copy()
-    return image, label
 
-def random_rotate(image, label):
-    angle = np.random.randint(-20, 20)
-    image = ndimage.rotate(image, angle, order=0, reshape=False)
-    label = ndimage.rotate(label, angle, order=0, reshape=False)
-    return image, label
 
 # ------------------ Dataset class ------------------
 class ISIC17Dataset(Dataset):
-    def __init__(self, path_Data, config, train=True):
+    def __init__(self, path_Data, config, train=False):
         super().__init__()
         self.train = train
-        img_dir = os.path.join(path_Data, "val/images")
-        mask_dir = os.path.join(path_Data, "val/masks")
+        if self.train:
+            img_dir = os.path.join(path_Data, "train/images")
+            mask_dir = os.path.join(path_Data, "train/masks")
+        else:
+            img_dir = os.path.join(path_Data, "val/images")
+            mask_dir = os.path.join(path_Data, "val/masks")
         self.transformer = config.train_transformer if train else config.test_transformer
 
         images_list = sorted([f for f in os.listdir(img_dir) if is_image_file(f)])
@@ -53,7 +43,7 @@ class ISIC17Dataset(Dataset):
                 os.path.join(mask_dir, mask_name)
             ])
 
-        print(f"✅ Loaded {len(self.data)} image-mask pairs from {img_dir}")
+        print(f" Loaded {len(self.data)} image-mask pairs from {img_dir}")
 
     def __getitem__(self, idx):
         img_path, mask_path = self.data[idx]
@@ -61,12 +51,6 @@ class ISIC17Dataset(Dataset):
         img = np.array(Image.open(img_path).convert("RGB"))
         mask = np.expand_dims(np.array(Image.open(mask_path).convert("L")), axis=2) / 255.0
 
-        # --------- Augmentation فقط برای train ---------
-        if self.train:
-            if random.random() > 0.5:
-                img, mask = random_rot_flip(img, mask)
-            elif random.random() > 0.5:
-                img, mask = random_rotate(img, mask)
 
         # --------- اعمال transformer (resize, normalize, toTensor) ---------
         img, mask = self.transformer((img, mask))
